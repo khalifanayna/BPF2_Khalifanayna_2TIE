1500cde202b478b3cd57390e8fb80559cb2bffef
khalifanayna <khalifa24ti@mahasiswa.pcr.ac.id>
2026-04-14 10:59:42 +0700

    gatau apa

--- react-nayna-2tie/2tie-app/src/pertemuan-4/TailwindCSS.jsx
+++ react-nayna-2tie/2tie-app/src/pertemuan-4/TailwindCSS.jsx
@@ -68,4 +68,12 @@ function ShadowEffects(){
             <p className="text-gray-600 mt-2">Lihat efek bayangan saat hover.</p>
         </div>
     )
+
+function Sizing(){
+    return (
+        <div className="bg-blue-200 w-40 h-40 flex items-center justify-center">
+            Box
+        </div>
+    )
+}
 }
